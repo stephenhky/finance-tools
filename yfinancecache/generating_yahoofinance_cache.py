ddb5dad243f0f2d79754a18c2b8099ae71270661
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-01-06 00:29:28 -0500

    added cache

---
+++ yfinancecache/generating_yahoofinance_cache.py
@@ -0,0 +1,47 @@
+
+import argparse
+import os
+import sys
+import time
+
+import pandas as pd
+import finsim.data
+
+
+def get_argparser():
+    argparser = argparse.ArgumentParser(description='Caching Yahoo finance data to disk.')
+    argparser.add_argument('startdate', type=str, help='start date')
+    argparser.add_argument('enddate', type=str, help='end data')
+    argparser.add_argument('cacheddir', type=str, help='cached directory')
+    argparser.add_argument('--slicebatch', type=int, default=50, help='batch size for each online retrieval')
+    argparser.add_argument('--localsymdf', default=os.path.dirname(__file__), help='location of allsymdf.h5')
+    return argparser
+
+
+if __name__ == '__main__':
+    # argument parsing
+    argparser = get_argparser()
+    args = argparser.parse_args()
+    
+    startdate = args.startdate
+    enddate = args.enddate
+    cacheddir = args.cacheddir
+    slicebatch = args.slicebatch
+    symdfloc = os.path.join(args.localsymdf, 'allsymdf.h5')
+    
+    # Read symbols
+    print('Reading all symbols from {}'.format(symdfloc), file=sys.stderr)
+    allsymdf = pd.read_hdf(symdfloc, 'fintable')
+    concernedsymdf = allsymdf[allsymdf['type']!='']
+    
+    # Generating cache
+    starttime = time.time()
+    finsim.data.preader.generating_cached_yahoofinance_data(
+        list(concernedsymdf['symbol']),
+        startdate, 
+        enddate, 
+        cacheddir,
+        slicebatch=slicebatch
+    )
+    endtime = time.time()
+    print('Time elapsed: {} sec'.format(endtime-starttime))
