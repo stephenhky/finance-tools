5fbc18acde2d22f65e661e2041fb451d07dd2642
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-02-15 16:20:13 -0500

    finportxlsx: putting portfolio to S3

---
+++ financial-portfolio-spreadsheet/finportxlsx.py
@@ -0,0 +1,55 @@
+
+import logging
+import os
+import json
+import random
+from datetime import datetime
+
+import boto3
+import pandas as pd
+
+
+def generate_filename():
+    name = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=20))
+    timestr = datetime.strftime(datetime.utcnow(), '%Y%m%d%H%M%SUTC%z')
+    return '{}_{}'.format(timestr, name)
+
+
+def spreadsheet_handler(event, context):
+    # getting config
+    config = json.load(open('config.json', 'r'))
+
+    # getting query
+    logging.info(event)
+    logging.info(context)
+    portfolio = json.loads(event['body'])
+
+    # generate pandas dataframe
+    df = pd.DataFrame(portfolio['components'])[['symbol', 'yield', 'volatility', 'weight', 'nbshares']]
+    df = df.sort_values(by=['weight'], ascending=False)
+
+    # generate Excel file
+    filename = generate_filename()
+    filename = filename+'.xlsx'
+    filepath = os.path.join('tmp', filename)
+    df.to_excel(filepath, index=False)
+
+    # copy to S3
+    s3_bucket = config['bucket']
+    s3_client = boto3.client('s3')
+    response = s3_client.upload_file(filepath, s3_bucket, filename)
+
+    event['filename'] = filename
+    event['response'] = response
+
+    # reference of a lambda output to API gateway: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-output-format
+    req_res = {
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
+    return req_res
+
+
+# References:
+# Writing files to S3: https://stackoverflow.com/questions/62522227/create-new-file-in-s3-using-aws-lambda-function
