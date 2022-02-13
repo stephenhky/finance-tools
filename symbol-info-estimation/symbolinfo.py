b1f44e75061b795c62c2424a7f46bc849b97e1e0
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-02-13 16:32:57 -0500

    using real S&P 500 index to calculate beta

--- symbol-info-estimation/symbolinfo.py
+++ symbol-info-estimation/symbolinfo.py
@@ -36,7 +36,7 @@ def symbol_handler(event, context):
     startdate = query['startdate']
     enddate = query['enddate']
     waittime = query.get('waittime', 1)
-    index = query.get('index', 'DJI')   # DJI index as the base.
+    index = query.get('index', '^GSPC')   # S&P 500 index as the base.
 
     # getting stock data
     symdf = waiting_get_yahoofinance_data(symbol, startdate, enddate, waittime=waittime)
@@ -85,7 +85,6 @@ def symbol_handler(event, context):
     req_res = {
         'isBase64Encoded': False,
         'statusCode': 200,
-        # 'headers': {'Content-Type': 'application/json'},
         'body': json.dumps(estimations)
     }
 
