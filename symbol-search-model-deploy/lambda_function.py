a556a34b74c0bc431bd9a0b1c90e6020a65a6eca
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-11-30 20:22:28 -0500

    streamlining the process

--- symbol-search-model-deploy/lambda_function.py
+++ symbol-search-model-deploy/lambda_function.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 import logging
@@ -8,7 +7,6 @@ from glob import glob
 
 import boto3
 import botocore
-
 
 model_filenames = [
     'hyperparameters.json',
@@ -37,47 +35,30 @@ def lambda_handler(event, context):
     starttime = time()
     if not os.path.isdir(os.path.join('/', 'mnt', 'efs', efsmodeldir)):
         os.makedirs(os.path.join('/', 'mnt', 'efs', efsmodeldir))
-    if not os.path.isdir(os.path.join('/', 'tmp', efsmodeldir)):
-        os.makedirs(os.path.join('/', 'tmp', efsmodeldir))
-    else:
-        for filepath in glob(os.path.join('/', 'tmp', efsmodeldir)):
-            os.remove(filepath)
-    s3_client = boto3.client('s3', 'us-east-1', config=botocore.config.Config(s3={'addressing_style':'path'}))
-    for model_filename in model_filenames:
-        logging.info('Source: {}'.format(modelfoldername+'/'+model_filename))
-        logging.info(' --> Destination: {}'.format(os.path.join('/', 'tmp', efsmodeldir, model_filename)))
-        print('Source: {}'.format(modelfoldername+'/'+model_filename))
-        print(' --> Destination: {}'.format(os.path.join('/', 'tmp', efsmodeldir, model_filename)))
 
+    s3_client = boto3.client('s3', 'us-east-1', config=botocore.config.Config(s3={'addressing_style': 'path'}))
+
+    logging.info('Copying to EFS directly...')
+    print('Copying to EFS directly...')
+    for model_filename in model_filenames:
+        logging.info('Source: {}'.format(modelfoldername + '/' + model_filename))
+        logging.info(' --> Destination: {}'.format(os.path.join('/', 'mnt', 'efs', efsmodeldir, model_filename)))
+        print('Source: {}'.format(modelfoldername + '/' + model_filename))
+        print(' --> Destination: {}'.format(os.path.join('/', 'mnt', 'efs', efsmodeldir, model_filename)))
         s3_client.download_file(
             s3_bucket,
-            modelfoldername+'/'+model_filename,
-            os.path.join('/', 'tmp', efsmodeldir, model_filename)
-        )
-    movetime = time()
-    logging.info('Copying to epheremel storage: {:.3f} sec'.format(movetime-starttime))
-    for model_filename in model_filenames:
-        logging.info('Source: {}'.format(os.path.join('/', 'tmp', efsmodeldir, model_filename)))
-        logging.info(' --> Destination: {}'.format(os.path.join('/', 'mnt', 'efs', efsmodeldir, model_filename)))
-        print('Source: {}'.format(os.path.join('/', 'tmp', efsmodeldir, model_filename)))
-        print(' --> Destination: {}'.format(os.path.join('/', 'mnt', 'efs', efsmodeldir, model_filename)))
-        shutil.move(
-            os.path.join('/', 'tmp', efsmodeldir, model_filename),
+            modelfoldername + '/' + model_filename,
             os.path.join('/', 'mnt', 'efs', efsmodeldir, model_filename)
         )
+
     endtime = time()
-    logging.info('Time elapsed: {:.3f} sec'.format(endtime-starttime))
+    logging.info('Time elapsed: {:.3f} sec'.format(endtime - starttime))
     print('Time elapsed: {:.3f} sec'.format(endtime - starttime))
-    logging.info('Deleted ephereel storage')
-    print('Deleted ephereel storage')
-    for filepath in glob(os.path.join('/', 'tmp', efsmodeldir)):
-        os.remove(filepath)
 
     return {
         'statusCode': 200,
         'body': 'Copied'
     }
-
 
 # Because EFS is connected to the Lambda function, the Lambda function has to be
 # in a VPC. In order to connected to S3, you must create a Gateway endpoint for that VPC.
